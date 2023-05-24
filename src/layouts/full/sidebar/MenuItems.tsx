5bf468683652b2bb6deff8892f7009a6d39a90ff
Longbt13 <long.bt205160@sis.hust.edu.vn>
2023-05-24 22:09:06 +0700

    new update

--- src/layouts/full/sidebar/MenuItems.tsx
+++ src/layouts/full/sidebar/MenuItems.tsx
@@ -1,11 +1,12 @@
 import {
-	IconAperture,
-	IconCopy,
-	IconLayoutDashboard,
-	IconLogin,
-	IconMoodHappy,
-	IconTypography,
-	IconUserPlus,
+  IconAperture,
+  IconCopy,
+  IconLayoutDashboard,
+  IconLogin,
+  IconMoodHappy,
+  IconTypography,
+  IconUserPlus,
+  IconUser,
 } from "@tabler/icons-react";
 import ListAltIcon from "@mui/icons-material/ListAlt";
 import SourceIcon from "@mui/icons-material/Source";
@@ -15,81 +16,87 @@ import NoteAddIcon from "@mui/icons-material/NoteAdd";
 import { uniqueId } from "lodash";
 
 const Menuitems = [
-	{
-		navlabel: true,
-		subheader: "Home",
-	},
+  {
+    navlabel: true,
+    subheader: "Home",
+  },
 
-	{
-		id: uniqueId(),
-		title: "Dashboard",
-		icon: IconLayoutDashboard,
-		href: "/",
-	},
-	{
-		navlabel: true,
-		subheader: "Order",
-	},
-	{
-		id: uniqueId(),
-		title: "Create Order",
-		icon: ListAltIcon,
-		href: "/order/create",
-	},
-	{
-		id: uniqueId(),
-		title: "Manage Order",
-		icon: SourceIcon,
-		href: "/order/manage",
-	},
-	{
-		navlabel: true,
-		subheader: "Shipment",
-	},
-	{
-		id: uniqueId(),
-		title: "Create Shipment",
-		icon: NoteAddIcon,
-		href: "/shipment/create",
-	},
-	{
-		id: uniqueId(),
-		title: "Manage Shipment",
-		icon: LocalShippingIcon,
-		href: "/shipment/manage",
-	},
-	{
-		navlabel: true,
-		subheader: "Auth",
-	},
-	{
-		id: uniqueId(),
-		title: "Login",
-		icon: IconLogin,
-		href: "/authentication/login",
-	},
-	{
-		id: uniqueId(),
-		title: "Register",
-		icon: IconUserPlus,
-		href: "/authentication/register",
-	},
-	{
-		navlabel: true,
-		subheader: "Extra",
-	},
-	{
-		id: uniqueId(),
-		title: "Icons",
-		icon: IconMoodHappy,
-		href: "/icons",
-	},
-	{
-		id: uniqueId(),
-		title: "Sample Page",
-		icon: IconAperture,
-		href: "/sample-page",
-	},
+  {
+    id: uniqueId(),
+    title: "Dashboard",
+    icon: IconLayoutDashboard,
+    href: "/",
+  },
+  {
+    navlabel: true,
+    subheader: "Order",
+  },
+  {
+    id: uniqueId(),
+    title: "Create Order",
+    icon: ListAltIcon,
+    href: "/order/create",
+  },
+  {
+    id: uniqueId(),
+    title: "Manage Order",
+    icon: SourceIcon,
+    href: "/order/manage",
+  },
+  {
+    navlabel: true,
+    subheader: "Shipment",
+  },
+  {
+    id: uniqueId(),
+    title: "Create Shipment",
+    icon: NoteAddIcon,
+    href: "/shipment/create",
+  },
+  {
+    id: uniqueId(),
+    title: "Manage Shipment",
+    icon: LocalShippingIcon,
+    href: "/shipment/manage",
+  },
+  {
+    navlabel: true,
+    subheader: "Auth",
+  },
+  {
+    id: uniqueId(),
+    title: "Login",
+    icon: IconLogin,
+    href: "/authentication/login",
+  },
+  {
+    id: uniqueId(),
+    title: "Register",
+    icon: IconUserPlus,
+    href: "/authentication/register",
+  },
+  {
+    id: uniqueId(),
+    title: "Profile",
+    icon: IconUser,
+    href: "/authentication/profile",
+  },
+  {
+    navlabel: true,
+    subheader: "Extra",
+  },
+  {
+    id: uniqueId(),
+    title: "Icons",
+    icon: IconMoodHappy,
+    href: "/icons",
+  },
+  {
+    id: uniqueId(),
+    title: "Sample Page",
+    icon: IconAperture,
+    href: "/sample-page",
+  },
 ];
 
 export default Menuitems;
--- src/modules/order/create/OrderSecondStep.tsx
+++ src/modules/order/create/OrderSecondStep.tsx
@@ -1,21 +1,84 @@
-import { Box, TextField } from "@mui/material";
+import { Box, TextField, Button } from "@mui/material";
+import Autocomplete from "@mui/material/Autocomplete";
 import * as React from "react";
-import { useAppDispatch } from "../../../redux/hooks";
+import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
 import { setNextDisabled } from "../../../redux/order/orderCreateSlice";
+import useNotify from "../../../hooks/useNotify";
+
+interface Product {
+  id: number;
+  name: string;
+  price: string;
+  material: string;
+}
+
+const products: Product[] = [
+  { id: 1, name: "Product A", price: "$10/kg", material: "Material X" },
+  { id: 2, name: "Product B", price: "$20/ton", material: "Material Y" },
+  { id: 3, name: "Product C", price: "$5/m", material: "Material Z" },
+];
+
 export default function OrderSecondStep() {
-	const dispatch = useAppDispatch();
-	return (
-		<Box>
-			<div>
-				<TextField
-					error
-					fullWidth
-					id="outlined-error-helper-text"
-					label="Error"
-					defaultValue="Hello World"
-					helperText="Incorrect entry."
-				/>
-			</div>
-		</Box>
-	);
+
+  const dispatch = useAppDispatch();
+  const { finishedStep } = useAppSelector((state) => state.orderCreate);
+  const { successNotify } = useNotify(); // using custom hook for notifications
+
+    const [selectedProduct, setSelectedProduct] = React.useState<Product>();
+    const [notes, setNotes] = React.useState("");
+    const [quantity, setQuantity] = React.useState<number>();
+    const [unit, setUnit] = React.useState("kg");
+
+  // when user selects an option from autocomplete, dispatch action to enable next step and show success notification
+  const handleSelect = (
+    event: React.ChangeEvent<{}>,
+    newValue: string | null
+  ) => {
+    setValue(newValue);
+    if (newValue) {
+      dispatch(setNextDisabled(false));
+      successNotify("Selected product ID."); // show success notification
+    }
+  };
+  console.log(`finish step: ${finishedStep}`);
+  const renderResult = () => {
+    if (finishedStep === 1) {
+      return (
+        <Box display="flex" flexDirection="column">
+          <Autocomplete
+            options={products}
+            getOptionLabel={(product) =>
+              `${product.id} - ${product.name} (${product.price})`
+            }
+            value={selectedProduct}
+            onChange={(event, product) => setSelectedProduct(product)}
+            renderInput={(params) => (
+              <TextField {...params} label="Product" required />
+            )}
+          />
+          <TextField
+            label="Notes"
+            value={notes}
+            onChange={(e) => setNotes(e.target.value)}
+          />
+          <TextField
+            label="Quantity"
+            type="number"
+            value={quantity ?? ""}
+            onChange={(e) => setQuantity(Number(e.target.value))}
+            required
+          />
+          <TextField />
+          {/* <Button variant="contained" color="primary" type="submit">
+            Submit
+          </Button> */}
+        </Box>
+      );
+    }
+  };
+  return(
+	<Box>
+		{renderResult()}
+	</Box>
+  );
 }
