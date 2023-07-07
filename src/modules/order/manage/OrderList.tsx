27c75c24fd3b0a4f198ca456094901fbf6732696
longgu02 <phamlong121120022@gmail.com>
2023-07-07 14:40:54 +0700

    fix: order list

--- src/modules/order/manage/OrderList.tsx
+++ src/modules/order/manage/OrderList.tsx
@@ -1,58 +1,59 @@
 import { Box, Grid, Stack } from "@mui/material";
 import OrderRow from "./OrderRow";
 interface OrderListProps {
-  orderList: Array<{
-    orderId: number;
-    productId: number;
-    customer: string;
-    suppliers: string[];
-    manufacturers: string[];
-    createDate: number;
-    status: number;
-    isPaid: boolean;
-    deposited: number;
-  }>;
+	orderList: Array<{
+		orderId: number;
+		productId: number;
+		customer: string;
+		suppliers: string[];
+		manufacturers: string[];
+		createDate: number;
+		status: number;
+		isPaid: boolean;
+		deposited: number;
+	}>;
 }
 
 export default function OrderList(props: OrderListProps) {
-  const { orderList } = props;
-  orderList.map(async (order) => console.log("1", order));
-  return (
-    <Box>
-      <Box
-        sx={{
-          paddingLeft: "20px",
-          paddingRight: "20px",
-          mb: 1,
-          fontWeight: 500,
-        }}
-      >
-        <Grid container>
-          <Grid item xs={2}>
-            Order ID
-          </Grid>
-          <Grid item xs={2}>
-            Product
-          </Grid>
-          <Grid item xs={2}>
-            Is Paid
-          </Grid>
-          <Grid item xs={2}>
-            Status
-          </Grid>
-          <Grid item xs={2}>
-            Date
-          </Grid>
-          <Grid item xs={2}></Grid>
-        </Grid>
-      </Box>
-      <Box>
-        <Stack spacing={1}>
-          {orderList.map((order, index) => (
-            <OrderRow order={order} key={order.orderId + index}/>
-          ))}
-        </Stack>
-      </Box>
-    </Box>
-  );
+	const { orderList } = props;
+	orderList.map(async (order) => console.log("1", order));
+	return (
+		<Box>
+			<Box
+				sx={{
+					paddingLeft: "20px",
+					paddingRight: "20px",
+					mb: 1,
+					fontWeight: 500,
+				}}
+			>
+				<Grid container>
+					<Grid item xs={2}>
+						Order ID
+					</Grid>
+					<Grid item xs={2}>
+						Product
+					</Grid>
+					<Grid item xs={2}>
+						Is Paid
+					</Grid>
+					<Grid item xs={2}>
+						Status
+					</Grid>
+					<Grid item xs={2}>
+						Date
+					</Grid>
+					<Grid item xs={2}></Grid>
+				</Grid>
+			</Box>
+			<Box>
+				<Stack spacing={1}>
+					{orderList.map((order, index) => {
+						if (order.orderId != 0)
+							return <OrderRow order={order} key={order.orderId + index} />;
+					})}
+				</Stack>
+			</Box>
+		</Box>
+	);
 }
